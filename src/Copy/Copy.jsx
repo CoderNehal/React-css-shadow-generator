50929cf1e69013a811da3622e3ac8252e86c303c
CoderNehal <nehalughade1221@gmail.com>
2021-06-06 23:29:40 +0530

    Added copy button and copy UX

--- src/Copy/Copy.jsx
+++ src/Copy/Copy.jsx
@@ -1,11 +1,33 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './Copy.css';
+import AssignmentIcon from '@material-ui/icons/Assignment';
+import Button from '@material-ui/core/Button';
+
+import AssignmentTurnedInIcon from '@material-ui/icons/AssignmentTurnedIn';
 const Copy = (props) => {
+	const [state, setstate] = useState(false);
+	const handleCopy = () => {
+		//Look here
+		var copyText = document.getElementById('myInput');
+		copyText.select();
+		copyText.setSelectionRange(0, 99999);
+		document.execCommand('copy');
+		alert('Copied the text: ' + copyText.value);
+		setstate(true);
+	};
 	return (
 		<div className='Copy'>
 			{`box-shadow: ${props.shadowStyles};`} <br />
 			{`-webkit-box-shadow: ${props.shadowStyles};`} <br />
 			{`-moz-box-shadow: ${props.shadowStyles};`}
+			<Button
+				variant='outlined'
+				size='medium'
+				id='copyButton'
+				onClick={handleCopy}
+				startIcon={state ? <AssignmentTurnedInIcon /> : <AssignmentIcon />}>
+				Copy
+			</Button>
 		</div>
 	);
 };
